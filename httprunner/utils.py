9257a5d4afd7b5c12e7132cc7f5f4452ad32a5b8
kfusac <kfusac@163.com>
2018-08-28 14:15:48 +0800

    feat: add context for testcase context and vaildator

--- httprunner/utils.py
+++ httprunner/utils.py
@@ -1,6 +1,7 @@
 # !/usr/bin/python
 # -*- coding: utf-8 -*-
 import itertools
+from collections import OrderedDict
 
 
 def gen_cartesian_product(*args):
@@ -37,3 +38,95 @@ def gen_cartesian_product(*args):
         product_list.append(product_item_dict)
 
     return product_list
+
+
+def convert_mappinglist_to_OrderedDict(mapping_list):
+    '''
+    convert mapping list to ordered dict
+    Args:
+        mapping_list (list):
+            [
+                {'a':1},
+                {'b':2}
+            ]
+    Returns:
+        OrderedDict: converted mapping to OrderedDict
+            OrderedDict(
+                {
+                    'a':1,
+                    'b':2
+                }
+            )
+    '''
+
+    ordered_dict = OrderedDict()
+    for map_dict in mapping_list:
+        OrderedDict.update(map_dict)
+
+    return ordered_dict
+
+
+def deep_update_dict(origin_dict, override_dict):
+    '''
+    update origin dict with override dict recursively
+        e.g.    origin_dict = {'a': 1, 'b': {'c': 2, 'd': 4}}
+                override_dict = {'b': {'c': 3}}
+        return: {'a':1, 'b': {'c': 3, 'd': 4}}
+    '''
+
+    if not override_dict:
+        return origin_dict
+
+    for key, value in override_dict.items():
+        if isinstance(value, dict):
+            tmp = deep_update_dict(origin_dict.get(key, {}), value)
+            origin_dict[key] = tmp
+        elif value is None:
+            continue
+        else:
+            origin_dict[key] = override_dict[key]
+
+    return origin_dict
+
+
+def get_uniform_comparator(comparator):
+    '''
+    convert comparator alias to uniform name
+    '''
+
+    if comparator in ['eq', 'equals', '==', 'is']:
+        return 'equals'
+    elif comparator in ['lt', 'less_than']:
+        return 'less_than'
+    elif comparator in ['le', 'less_than_or_equals']:
+        return 'less_than_or_equals'
+    elif comparator in ['gt', 'greater_than']:
+        return 'greater_than'
+    elif comparator in ['ge', 'greater_than_or_equals']:
+        return 'greater_than_or_equals'
+    elif comparator in ['ne', 'not_equals', '!=']:
+        return 'not_equals'
+    elif comparator in ['str_eq', 'string_equals']:
+        return 'string_equals'
+    elif comparator in ['len_eq', 'length_equals', 'count_eq']:
+        return 'length_equals'
+    elif comparator in [
+            'len_gt', 'count_gt', 'length_greater_than', 'count_greater_than'
+    ]:
+        return 'length_greater_than'
+    elif comparator in [
+            'len_ge', 'count_ge', 'length_greater_than_or_equals',
+            'count_greater_than_or_equals'
+    ]:
+        return 'length_greater_than_or_equals'
+    elif comparator in [
+            'len_lt', 'count_lt', 'length_less_than', 'count_less_than'
+    ]:
+        return 'length_less_than'
+    elif comparator in [
+            'len_le', 'count_le', 'length_less_than_or_equals',
+            'count_less_than_or_equals'
+    ]:
+        return 'length_less_than_or_equals'
+    else:
+        return comparator
